2450583e08274031ca1116ec6abc8b6e3442eabc
Amey8883 <139483839+Amey8883@users.noreply.github.com>
2024-03-16 14:09:30 +0530

    codes

---
+++ Seasons.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Seasons{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter a month: ");
+		String month = sc.next().toUpperCase();
+		switch(month)
+		{
+		case "OCT":
+		case "NOV":
+		case "DEC":
+		case "JAN":System.out.println(month + "it's Winter.");
+			break;
+		case "FEB":
+		case "MAR":
+		case "APR":
+		case "MAY":System.out.println(month + "its Summer");
+			break;
+		case "Jun":
+		case "JUL":
+		case "AUG":
+		case "SEP":System.out.println(month + "its Rainy");
+			break;
+		default:System.out.println("Invalid Input");
+		}
+	}
+}
