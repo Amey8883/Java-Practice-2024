77f295dc1c6d7d60a43df1522c3577be991a03f4
Amey8883 <139483839+Amey8883@users.noreply.github.com>
2024-03-18 20:14:44 +0530

    code

---
+++ ForLoop.java
@@ -0,0 +1,66 @@
+class ForLoop{
+public static void main(String[] args) {
+
+	// for(char ch='A';ch<='Z';ch++)
+	// {
+	// 	System.out.print(ch+" ");
+	// }
+
+	// for (char ch = 97;ch<=122;ch++) 
+	// {
+	// 	System.out.println(ch+" ");
+	// }
+
+	// for (char ch=48;ch<='9';ch++ )
+	// {
+	// 	System.out.println(ch+" ");
+	// }
+
+	// for(char ch='Z';ch>='A';ch--)
+	// {
+	// 	System.out.println(ch+" ");
+	// }
+
+	// for (char ch = 122;ch>=97;ch--) 
+	// {
+	// 	System.out.println(ch+" ");
+	// }
+
+	// for (char ch='9';ch<=48;ch-- )
+	// {
+	// 	System.out.print(ch+" ");
+	// }
+
+	// for(int i=1;i<=100;i++)
+	// {
+	// 	System.out.print(i+" ");
+	// }
+
+	// for(char ch='A';ch<='Z';ch++)
+	// {
+	// 	System.out.println(ch+" ");
+	// }
+
+
+    // for(char ch='a';ch<='z';ch++)
+	// {
+	// 	System.out.println(ch+" ");
+	// }
+
+	// for(int i=0;i<=100;i++)
+	// {
+	// 	if(i%2==0){
+	// 		System.out.print(i+" ");
+	// 	}
+	// }
+
+	for(int i=0;i<=100;i++)
+	{
+		if(i%2!=0){
+			System.out.print(i+" ");
+		}
+	}
+
+
+}
+}
