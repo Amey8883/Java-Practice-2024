383b67a4df017f886be322cfae1c39e4460628f0
Amey8883 <139483839+Amey8883@users.noreply.github.com>
2024-03-19 15:06:39 +0530

    code

---
+++ TrianglePattern6.java
@@ -0,0 +1,20 @@
+class TrianglePattern6{
+	public static void main(String[] args) {
+		char ch = 'a';
+		for(int i = 1; i <= 5; i++){
+		 for(int j = 1; j <= i; j++){
+				System.out.print(ch+" ");
+				ch++;
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+
+// a
+// b c
+// d e f
+// g h i j
+// k l m n o
