4088e6bf92a0e1eb62e1025a18bad634f54c905c
Amey8883 <139483839+Amey8883@users.noreply.github.com>
2024-06-17 21:22:12 +0530

    code

---
+++ SecondSmallestElementInArray.java
@@ -0,0 +1,42 @@
+// Second Smallest Element in the Array
+import java.util.*;
+class SecondSmallestInElement
+{
+
+static{
+	System.out.println("This Program is to Fetch the 2nd Small Element in Array.");
+}
+public static void main(String[] args) 
+{
+		
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter size");
+	int size=sc.nextInt();
+
+	System.out.println("Enter charactes");
+	int[] a=new int[size];
+
+	for(int i=0;i<size;i++)
+	{
+		a[i]=sc.nextInt();
+	}
+
+	int temp=0;
+	for(int i=0; i<size; i++)
+	{
+		for(int j=i+1; j<size;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	int temp1=a[1];
+
+	System.out.println(Arrays.toString(a));
+	System.out.println("Second Smallest elemnt is : "+temp1);
+}
+}
